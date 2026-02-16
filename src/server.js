f647bd8c7ae9eb5032288a055eb346970ff38b19
Christine Nicolas <mcmnicolas@tip.edu.ph>
2026-02-16 22:47:42 +0800

    Refactor server routes to serve views and add pages
    Updated routes to serve HTML files from the views directory and added new routes for about and contact pages.

--- src/server.js
+++ src/server.js
@@ -1,29 +1,38 @@
+// src/server.js
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
 
-const app = express();
+const app = express(); // <-- this is essential!
 
 // Serve static files
 app.use(express.static(path.join(__dirname, '../public')));
 
-// Home route
+// Home page
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'));
+  res.sendFile(path.join(__dirname, '../views/index.html'));
 });
 
-// Blog route (HTML page)
+// About page
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, '../views/about.html'));
+});
+
+// Contact page
+app.get('/contact', (req, res) => {
+  res.sendFile(path.join(__dirname, '../views/contact.html'));
+});
+
+// Blog page
 app.get('/blog', (req, res) => {
   res.sendFile(path.join(__dirname, '../views/blog.html'));
 });
 
-// API route to serve blog posts as JSON
+// API route to get blog posts
 app.get('/api/posts', (req, res) => {
-  const postsPath = path.join(__dirname, '../data/posts.json');
+  const postsPath = path.join(__dirname, '../data/posts.json'); // go up from src/ to project root
   fs.readFile(postsPath, 'utf8', (err, data) => {
-    if (err) {
-      return res.status(500).json({ error: 'Failed to read posts' });
-    }
+    if (err) return res.status(500).json({ error: 'Cannot read posts' });
     res.json(JSON.parse(data));
   });
 });
